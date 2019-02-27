1402f462ae8f6d1180c3fad9f96c75173876c74d
Hayden <hayden@whitewaterfoundry.com>
2019-02-27 12:20:51 -0500

    Fix link to CentOS kickstart file

--- create-targz.sh
+++ create-targz.sh
@@ -13,7 +13,7 @@ KSFILE="https://raw.githubusercontent.com/WhitewaterFoundry/sig-cloud-instance-b
 
 #upstream enterprise boot ISO
 #BOOTISO="http://mirror.centos.org/centos/7.5.1804/os/x86_64/images/boot.iso"
-#KSFILE="https://raw.githubusercontent.com/CentOS/sig-cloud-instance-build/master/docker/centos-7.ks"
+#KSFILE="https://raw.githubusercontent.com/CentOS/sig-cloud-instance-build/master/docker/centos-7-x86_64.ks"
 
 #ARM64
 #BOOTISO="http://vault.centos.org/altarch/7.3.1611/os/aarch64/images/boot.iso"
