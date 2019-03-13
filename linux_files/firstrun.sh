81c1b577c2145581b462f6048a7db3654bc5db3a
Hayden <hayden@whitewaterfoundry.com>
2019-03-13 16:51:49 -0400

    Create firstrun.sh

---
+++ linux_files/firstrun.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+wHomeWinPath=$(cmd.exe /c 'echo %HOMEDRIVE%%HOMEPATH%' 2>&1 | tr -d '\r')
+wHome=$(wslpath -u "${wHomeWinPath}")
+
+echo "Updating packages"
+yum update
+
+echo "Upgrading packages"
+yum upgrade
+
+echo "Installing Pageant"
+mkdir ${wHome}/.pageant
+cp /opt/pageant/* ${wHome}/.pageant
+
+echo "Configuring Pageant Integration"
+string = "eval $(" + ${wHome} + "/.pageant/weasel-pageant -r)"
+echo "#!/bin/bash" > /etc/profile.d/pageant.sh
+echo $string > /etc/profile.d/pageant.sh
+
+echo "Installing VcXsrv"
+mkdir ${wHome}/.vcxsrv
+cp /opt/vcxsrv/vcxsrv-installer.exe ${wHome}/.vcxsrv/vcxsrv-installer.exe
