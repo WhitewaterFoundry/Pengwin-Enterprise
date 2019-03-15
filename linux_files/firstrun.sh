21a68fb99aaeb28ffb8d28e3b1ea4008fec94c6e
Kim (grufwub) <grufwub@gmail.com>
2019-03-15 20:47:00 +0000

    Move wslu install to firstrun + fix wget usage
    Signed-off-by: Kim (grufwub) <grufwub@gmail.com>

--- linux_files/firstrun.sh
+++ linux_files/firstrun.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-wHomeWinPath=$(cmd.exe /c 'echo %HOMEDRIVE%%HOMEPATH%' 2>&1 | tr -d '\r')
-wHome=$(wslpath -u "${wHomeWinPath}")
+echo "Downloading and installing wslu repository"
+curl -L https://packagecloud.io/install/repositories/whitewaterfoundry/wslu/script.rpm.sh | bash
 
-echo "Updating packages"
+echo "Updating repositories"
 yum update
 
 echo "Upgrading packages"
-yum upgrade
+yum upgrade -y
 
-echo "Installing Pageant"
+echo "Installing wslu"
+yum install wslu -y
+
+echo "Getting required application paths"
+wHomeWinPath=$(cmd.exe /c 'echo %HOMEDRIVE%%HOMEPATH%' 2>&1 | tr -d '\r')
+echo "wHomeWinPath = ${wHomeWinPath}"
+wHome=$(wslpath -u "${wHomeWinPath}")
+echo "wHome = ${wHome}"
+
+echo "Installing Pageant to ${wHome}/.pageant"
 mkdir ${wHome}/.pageant
 cp /opt/pageant/* ${wHome}/.pageant
 
@@ -18,7 +27,7 @@ string = "eval $(" + ${wHome} + "/.pageant/weasel-pageant -r)"
 echo "#!/bin/bash" > /etc/profile.d/pageant.sh
 echo $string > /etc/profile.d/pageant.sh
 
-echo "Installing VcXsrv"
+echo "Installing VcXsrv to ${wHome}/.vcxsrv"
 mkdir ${wHome}/.vcxsrv
 cp /opt/vcxsrv/vcxsrv-installer.exe ${wHome}/.vcxsrv/vcxsrv-installer.exe
 
