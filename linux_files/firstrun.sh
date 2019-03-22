68c0cd98e55d869fc5a7287777afe84b61f556cc
kim (grufwub) <grufwub@gmail.com>
2019-03-22 20:28:56 +0000

    Neaten up + improve 'required app paths' code
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- linux_files/firstrun.sh
+++ linux_files/firstrun.sh
@@ -12,16 +12,18 @@ sudo yum install wslu -y
 
 # Get required application paths
 echo "Getting required application paths"
-wHomeWinPath=$(cmd.exe /c 'echo %HOMEDRIVE%%HOMEPATH%' 2>&1 | tr -d '\r')
-echo "wHomeWinPath = ${wHomeWinPath}"
-wHome=$(wslpath -u "${wHomeWinPath}")
-echo "wHome = ${wHome}"
+wHomeDrive="$(cmd.exe /C 'echo %HOMEDRIVE%' | tr -d '\r')"
+HomeDrive="/mnt/$(echo "${wHomeDrive}" | sed 's|\:||g' | tr '[:upper:]' '[:lower:]')"
+wHomePath="$(cmd.exe /C 'echo %HOMEPATH%' | tr -d '\r')"
+HomePath="$(echo "${wHomePath}" | sed 's|\\|\/|g')"
+wHome="$wHomeDrive$wHomePath"
+Home="$HomeDrive$HomePath"
 
 # Install Putty's Pageant and weasel-pageant
-echo "Installing Pageant (with weasel-pageant WSL integration) to ${wHome}/.pageant"
-mkdir "${wHome}/.pageant"
-cp /opt/pageant/* "${wHome}/.pageant"
-chmod +x "${wHome}/.pageant/weasel-pageant"
+echo "Installing Pageant (with weasel-pageant WSL integration) to ${Home}/.pageant"
+mkdir "${Home}/.pageant"
+cp /opt/pageant/* "${Home}/.pageant"
+chmod +x "${Home}/.pageant/weasel-pageant"
 
 # Add Pageant startup registry entry
 echo "Configuring Pageant to execute on startup"
@@ -29,30 +31,30 @@ TMPDIR=$(mktemp -d)
 cat << EOF >> "${TMPDIR}/Install.reg"
 [HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run]
 @="Pageant startup"
-"Pageant"="${wHomeWinPath}\\.pageant\\pageant.exe"
+"Pageant"="${wHome}\\.pageant\\pageant.exe"
 EOF
-cp "${TMPDIR}/Install.reg" "$(cmd.exe /C 'echo %TEMP%' 2>&1 | tr -d '\r' | sed 's|\\|\/|g' | sed 's|C\:|\/mnt\/c|g')"
+cp "${TMPDIR}/Install.reg" "$HomeDrive$(cmd.exe /C 'echo %TEMP%' 2>&1 | tr -d '\r' | sed 's|\\|\/|g' | sed 's|.\:||g')"
 cmd.exe /C "Reg import %TEMP%\Install.reg"
 rm -rf "${TMPDIR}"
 
 # Add profile.d start script for weasel-pageant
 echo "Configuring weasel-pageant WSL integration"
-string="eval \$(\""${wHome}/.pageant/weasel-pageant"\" -r --helper \"${wHome}/.pageant\")"
+string="eval \$(\""${Home}/.pageant/weasel-pageant"\" -r --helper \"${Home}/.pageant\")"
 sudo bash -c 'cat > /etc/profile.d/pageant.sh' << EOF
 #!/bin/bash
 $string
 EOF
 
 # Unzip vcxsrv to user's directory
-echo "Installing vcxsrv to ${wHome}/.vcxsrv"
-mkdir "${wHome}/.vcxsrv"
-unzip /opt/vcxsrv/vcxsrv.zip -d "${wHome}/.vcxsrv" > /dev/null 2>&1
+echo "Installing vcxsrv to ${Home}/.vcxsrv"
+mkdir "${Home}/.vcxsrv"
+unzip /opt/vcxsrv/vcxsrv.zip -d "${Home}/.vcxsrv" > /dev/null 2>&1
 
 # Add profile.d start script for vcxsrv
 echo "Configuring vcxsrv integration"
 sudo bash -c 'cat > /etc/profile.d/vcxsrv.sh' << EOF
 #!/bin/bash
-'$wHome/.vcxsrv/vcxsrv.exe' :0 -silent-dup-error -multiwindow &> /dev/null &
+'$Home/.vcxsrv/vcxsrv.exe' :0 -silent-dup-error -multiwindow &> /dev/null &
 disown
 EOF
 
