41388cb84cdc35b8fa963cbf659eb9453ebbc16c
kim (grufwub) <grufwub@gmail.com>
2019-03-22 23:02:22 +0000

    Further improve 'required paths' code in firstrun
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- linux_files/firstrun.sh
+++ linux_files/firstrun.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-# Add wslu repository and install wslu
-echo "Downloading and installing wslu repository"
-curl -L https://packagecloud.io/install/repositories/whitewaterfoundry/wslu/script.rpm.sh | sudo bash
-echo "Updating repositories"
-sudo yum update
-echo "Upgrading packages"
-sudo yum upgrade -y
-echo "Installing wslu"
-sudo yum install wslu -y
-
-# Get required application paths
 echo "Getting required application paths"
+TMPDIR=$(mktemp -d)
 wHomeDrive="$(cmd.exe /C 'echo %HOMEDRIVE%' | tr -d '\r')"
 HomeDrive="/mnt/$(echo "${wHomeDrive}" | sed 's|\:||g' | tr '[:upper:]' '[:lower:]')"
 wHomePath="$(cmd.exe /C 'echo %HOMEPATH%' | tr -d '\r')"
 HomePath="$(echo "${wHomePath}" | sed 's|\\|\/|g')"
 wHome="$wHomeDrive$wHomePath"
 Home="$HomeDrive$HomePath"
+
+# Add wslu repository and install wslu
+echo "Downloading and installing wslu repository"
+curl -L https://packagecloud.io/install/repositories/whitewaterfoundry/wslu/script.rpm.sh -o "${TMPDIR}/script.sh" > /dev/null 2>&1
+sudo bash "${TMPDIR}/script.sh"
+
+echo "Updating repositories"
+sudo yum update
+echo "Upgrading packages"
+sudo yum upgrade -y
+echo "Installing wslu"
+sudo yum install wslu -y
 
 # Install Putty's Pageant and weasel-pageant
 echo "Installing Pageant (with weasel-pageant WSL integration) to ${Home}/.pageant"
@@ -27,10 +29,9 @@ chmod +x "${Home}/.pageant/weasel-pageant"
 
 # Add Pageant startup registry entry
 echo "Configuring Pageant to execute on startup"
-TMPDIR=$(mktemp -d)
 cat << EOF >> "${TMPDIR}/Install.reg"
+Windows Registry Editor Version 5.00
 [HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run]
-@="Pageant startup"
 "Pageant"="${wHome}\\.pageant\\pageant.exe"
 EOF
 cp "${TMPDIR}/Install.reg" "$HomeDrive$(cmd.exe /C 'echo %TEMP%' 2>&1 | tr -d '\r' | sed 's|\\|\/|g' | sed 's|.\:||g')"
