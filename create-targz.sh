e82d6a1b367814639fc5fe596210ecef9644cd89
Carlos Rafael Ramirez <crramirez@gmail.com>
2020-01-14 21:17:43 +0100

    Update project structure

--- create-targz.sh
+++ create-targz.sh
@@ -2,13 +2,16 @@ set -e
 
 #declare variables
 ORIGINDIR=$(pwd)
+TMPDIR=$(mktemp -d)
 BUILDDIR=$(mktemp -d)
 
 #enterprise boot ISO
 BOOTISO="/root/rhel-workstation-7.7-x86_64-dvd.iso"
 
-#rhel kickstart file
-KSFILE="/root/Pengwin-Enterprise/rhel-7.ks"
+#enterprise Docker kickstart file
+KSFILE="https://raw.githubusercontent.com/WhitewaterFoundry/sig-cloud-instance-build/master/docker/rhel-7.ks"
+
+cd $TMPDIR
 
 #make sure we are up to date
 sudo yum -y update
@@ -19,8 +22,17 @@ sudo yum -y install libvirt lorax virt-install libvirt-daemon-config-network lib
 #restart libvirtd for good measure
 sudo systemctl restart libvirtd
 
+#download enterprise boot ISO
+if [[ ! -f /tmp/install.iso ]] ; then
+  sudo cp "${BOOTISO}" -o /tmp/install.iso
+fi
+#download enterprise Docker kickstart file
+curl $KSFILE -o install.ks
+
+rm -f /var/tmp/install.tar.xz
+
 #build intermediary rootfs tar
-sudo livemedia-creator --make-tar --iso=$BOOTISO --image-name=install.tar.xz --ks=$KSFILE --releasever "7"
+sudo livemedia-creator --make-tar --iso=/tmp/install.iso --image-name=install.tar.xz --ks=install.ks --releasever "7"
 
 #open up the tar into our build directory
 tar -xvf /var/tmp/install.tar.xz -C $BUILDDIR
@@ -41,7 +53,10 @@ cd $BUILDDIR
 tar --ignore-failed-read -czvf $ORIGINDIR/install.tar.gz *
 
 #go home
-#cd $ORIGINDIR
+cd $ORIGINDIR
 
 #clean up
 sudo rm -r $BUILDDIR
+sudo rm -r $TMPDIR
+sudo rm /tmp/install.iso
+sudo rm /var/tmp/install.tar.xz
