d51c1ae3450453a81a30323d9eaba4369cd75f15
Carlos Ramirez <crramirez@gmail.com>
2021-08-12 22:42:02 +0200

    Pengwin Enterprise 8

--- DistroLauncher/DistributionInfo.h
+++ DistroLauncher/DistributionInfo.h
@@ -16,7 +16,7 @@ namespace DistributionInfo
     const std::wstring Name = L"WLE";
 
     // The title bar for the console window while the distribution is installing.
-    const std::wstring WindowTitle = L"Pengwin Enterprise";
+    const std::wstring WindowTitle = L"Pengwin Enterprise 7";
 
 	// Set root user password
 	bool SetRootPassword();
