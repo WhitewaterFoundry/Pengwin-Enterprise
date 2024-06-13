60b9904dde4c54ac2b3d3d008334070abad0d58a
Carlos Ramirez <crramirez@gmail.com>
2024-06-13 14:24:33 +0200

    Replace SystemD for systemd

--- LauncherShared/messages.h
+++ LauncherShared/messages.h
@@ -76,29 +76,29 @@
 // MessageText:
 //
 // Launches or configures a Linux distribution.
-// 
+//
 // Usage:
 //     <no args>
 //         Launches the user's default shell in the user's home directory.
-// 
+//
 //     --systemd, -s
-//         Launches the user's default shell in the user's home directory via SystemD.
-// 
+//         Launches the user's default shell in the user's home directory via systemd.
+//
 //     install [--root]
 //         Install the distribuiton and do not launch the shell when complete.
 //           --root
 //               Do not create a user account and leave the default user set to root.
-// 
+//
 //     run, -c <command line>
 //         Run the provided command line in the current working directory. If no
 //         command line is provided, the default shell is launched.
-// 
+//
 //     config [setting [value]]
 //         Configure settings for this distribution.
 //         Settings:
 //           --default-user <username>
 //               Sets the default user to <username>. This must be an existing user.
-// 
+//
 //     help
 //         Print usage information.
 //
